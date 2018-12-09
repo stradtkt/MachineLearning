f2a490c74d700cb9eaa10e397e3124c080147923
Kevin Stradtman <stradtkt@gmail.com>
2018-12-08 21:39:16 -0500

    Added simple linear regression

---
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  8 20:19:24 2018
+
+@author: Kevin Stradtman
+@subject: Simple Linear Regression
+"""
+# Equation
+# y = b0 + b1 * x1
+# y = dependent variable 
+# x = independent variable
+# salary for experience the more experience you have the higher salary you will have
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("Salary_Data.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
+
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+
+#Predict the Test set results
+y_pred = regressor.predict(X_test)
+
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title("Salary vs Experience (Training Set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
