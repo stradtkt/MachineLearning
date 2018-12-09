5f446ab4ab65b08ac59701667e9603ca9440461b
Kevin Stradtman <stradtkt@gmail.com>
2018-12-08 20:11:17 -0500

    Initial Commit

---
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -0,0 +1,19 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("Data.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
